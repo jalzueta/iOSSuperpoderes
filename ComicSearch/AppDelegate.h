e2529e77ca7b3233ba13128058084f515998758d
Javi Alzueta <jalzuetap@gmail.com>
2015-05-15 16:52:25 +0200

    Initial Commit

---
+++ ComicSearch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ComicSearch
+//
+//  Created by Javi Alzueta on 15/5/15.
+//  Copyright (c) 2015 FillinGAPPs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ComicSearch/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  ComicSearch
+//
+//  Created by Javi Alzueta on 15/5/15.
+//  Copyright (c) 2015 FillinGAPPs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
